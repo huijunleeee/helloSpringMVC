b550cc86573f08e0323cf393b2accf86a4b100f6
huijunleeee <hjl3740@naver.com>
2025-04-19 22:28:13 +0900

    add spring form functionality

--- src/main/java/kr/ac/hansung/cse/model/Offer.java
+++ src/main/java/kr/ac/hansung/cse/model/Offer.java
@@ -1,5 +1,9 @@
 package kr.ac.hansung.cse.model;
 
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,12 +12,17 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor // 기본 생성자 설정
+@NoArgsConstructor
 public class Offer {
     private int id;
+
+    @Size(min=2, max=100, message = "Name must be between 2 and 100 chars")
     private String name;
+
+    @Email(message="Please provide a valid email address")
+    @NotEmpty(message="The email address cannot be empty")
     private String email;
+
+    @Size(min=5, max=100, message="Text must be between 5 and 100 chars")
     private String text;
-
-
 }
